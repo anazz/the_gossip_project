f3e44f52dae783bd4f2ea7f4b9309e7c1fd88a88
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-04-30 18:18:31 +0200

    ajout header creation de gossips controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+ root to: 'static_pages#home'
+
+ resources :photos
+
+  # get 'gossips/new', to: 'gossips#new'
+  # get 'gossips/create', to: 'gossips#create'
+  # get 'gossips/edit', to: 'gossips#edit'
+  # get 'gossips/update', to: 'gossips#update'
+  # get 'gossips/destroy', to: to: 'gossips#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
